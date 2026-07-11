fa6eb881260cf0a0159b68e50e5e423608459b6b
Nahid03210 <95604088+Nahid03210@users.noreply.github.com>
2026-07-11 17:02:11 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMYbpyAlaSFd9eC-yxZLz8-kiCU9JE5JU",
+  authDomain: "make-money-bot-52a90.firebaseapp.com",
+  projectId: "make-money-bot-52a90",
+  storageBucket: "make-money-bot-52a90.firebasestorage.app",
+  messagingSenderId: "654181544752",
+  appId: "1:654181544752:web:c808d0497217e786430ef5"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
